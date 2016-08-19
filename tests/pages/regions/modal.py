bacd03a459a25ead1a6f220c2a5cabd7b25ceba4
alexgibson <agibson@mozilla.com>
2016-08-19 11:11:36 +0100

    Update functional tests to use Selenium 3.0.1

--- tests/pages/regions/modal.py
+++ tests/pages/regions/modal.py
@@ -19,4 +19,4 @@ class Modal(Region):
 
     @property
     def is_displayed(self):
-        return self.is_element_displayed(*self._root_locator)
+        return self.page.is_element_displayed(*self._root_locator)
